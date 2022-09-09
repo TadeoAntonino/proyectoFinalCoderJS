1a4cf52388a6e4af88f77ed884c96ab007bc7b23
Tadeo Antonino <tadeoantonino98@gmail.com>
2022-09-09 18:50:10 -0300

    cambios

---
+++ componentes/sweetAlert.js
@@ -0,0 +1,34 @@
+// SweetAlert 
+
+export const sweetAlert = async () => {
+    await Swal.fire({
+    title: "Registrese",
+    html: `<input type="text" id="login" class="swal2-input" placeholder="Username">
+    <input type="password" id="password" class="swal2-input" placeholder="Password">`,
+    confirmButtonText: "Registrarme",
+    focusConfirm: false,
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+  
+    preConfirm: () => {
+      const login = Swal.getPopup().querySelector("#login").value;
+      const password = Swal.getPopup().querySelector("#password").value;
+      if (!login || !password) {
+        Swal.showValidationMessage(`Please enter login and password`);
+      }
+      return { login: login, password: password };
+    },
+  }).then((result) => {
+    const user = JSON.stringify(result.value.login)
+    const pass = JSON.stringify(result.value.password)
+  
+    localStorage.setItem('usuario', user)
+    localStorage.setItem('contrasena', pass)
+    Swal.fire(
+      `
+      Login: ${result.value.login}
+      Password: ${result.value.password}
+    `.trim()
+    );
+  });
+  };
--- js/calculadoraPresupuesto.js
+++ js/calculadoraPresupuesto.js
@@ -1,57 +1,31 @@
-import data from "../content/data.json";
+import { sweetAlert } from "../componentes/sweetAlert.js";
 
-// SweetAlert 
 
-sweetAlert = async () => {
-  await Swal.fire({
-  title: "Registrese",
-  html: `<input type="text" id="login" class="swal2-input" placeholder="Username">
-  <input type="password" id="password" class="swal2-input" placeholder="Password">`,
-  confirmButtonText: "Registrarme",
-  focusConfirm: false,
-  allowOutsideClick: false,
-  allowEscapeKey: false,
+  fetch("../content/data.json")
+  .then(res => res.json())
+  .then(data => {
 
-  preConfirm: () => {
-    const login = Swal.getPopup().querySelector("#login").value;
-    const password = Swal.getPopup().querySelector("#password").value;
-    if (!login || !password) {
-      Swal.showValidationMessage(`Please enter login and password`);
-    }
-    return { login: login, password: password };
-  },
-}).then((result) => {
-  const user = JSON.stringify(result.value.login)
-  const pass = JSON.stringify(result.value.password)
-
-  localStorage.setItem('usuario', user)
-  localStorage.setItem('contrasena', pass)
-  Swal.fire(
-    `
-    Login: ${result.value.login}
-    Password: ${result.value.password}
-  `.trim()
-  );
-});
-};
-
-// Funcion calculadora
+    // Funcion calculadora
 
 const { TipoEstructura, TipoPiso, TipoPared, TipoTecho } = data
 
 function calcular(e) {
   e.preventDefault();
     let metrosCuadrados = document.getElementById("mCuadrados").value;
-  
+
     let materialElegidoE = document.getElementById("materialEstructura").value;
-  
+    let materialElegidoPi = document.getElementById("materialPiso").value;
+    let materialElegidoPa = document.getElementById("materialPared").value;
+    let materialElegidoT = document.getElementById("materialTecho").value;
+
+    let divPrecio = document.getElementById("presupuestoEstimado");
+
     let precioMaterialE = TipoEstructura.find(
       (element) => element.material === materialElegidoE
     ).precio;
   
     let precioE = metrosCuadrados * precioMaterialE;
   
-    let materialElegidoPi = document.getElementById("materialPiso").value;
   
     let precioMaterialPi = TipoPiso.find(
       (element) => element.material === materialElegidoPi
@@ -59,15 +33,13 @@ function calcular(e) {
   
     let precioPi = metrosCuadrados * precioMaterialPi;
   
-    let materialElegidoPa = document.getElementById("materialPared").value;
-  
     let precioMaterialPa = TipoPared.find(
       (element) => element.material === materialElegidoPa
     ).precio;
   
     let precioPa = metrosCuadrados * precioMaterialPa;
   
-    let materialElegidoT = document.getElementById("materialTecho").value;
+
   
     let precioMaterialT = TipoTecho.find(
       (element) => element.material === materialElegidoT
@@ -82,15 +54,13 @@ function calcular(e) {
 
     localStorage.setItem("presupuesto", precioJSON)
   
-    let divPrecio = document.getElementById("presupuestoEstimado");
+    
       
     divPrecio.innerHTML = `<h2>El precio estimado es de: U$D ${precioTotal}</h2>`;
   }
 
-  const btnCalcular = document.getElementById("btnCalcular");
-const calculator = document.getElementById("calculator")
-const usuario = localStorage.getItem("usuario")
-const contrasena = localStorage.getItem("contrasena")
+  const calculator = document.getElementById("calculator")
+
 
   sweetAlert();
 
@@ -100,9 +70,6 @@ const contrasena = localStorage.getItem("contrasena")
   }, false);
   
 
-
-  fetch("./content/data.json")
-  .then(res => res.json())
-  .then(data => console.log(data))
+  })
 
 
